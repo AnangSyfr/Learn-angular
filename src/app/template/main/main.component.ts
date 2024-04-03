7b657183710a3507df2308dfded7fcadb8bfb5fd
Anang Syaifur <anangsyfr123@gmail.com>
2024-04-03 08:24:05 +0700

    feat: add news component

--- src/app/template/main/main.component.ts
+++ src/app/template/main/main.component.ts
@@ -3,6 +3,7 @@ import { AboutUsComponent } from "@components/about-us/about-us.component"
 import { BannerComponent } from "@components/banner/banner.component"
 import { HeaderMenuComponent } from "@components/header-menu/header-menu.component"
 import { HeaderComponent } from "@components/header/header.component"
+import { NewsComponent } from "@components/news/news.component"
 
 @Component({
     selector: "app-main",
@@ -12,6 +13,7 @@ import { HeaderComponent } from "@components/header/header.component"
         HeaderMenuComponent,
         BannerComponent,
         AboutUsComponent,
+        NewsComponent,
     ],
     templateUrl: "./main.component.html",
     styleUrl: "./main.component.css",
